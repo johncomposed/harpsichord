e992201755629cc5f28512cf20b8f3db7c78727d
John Williams <johncomposed@gmail.com>
2015-10-21 14:23:15 -0400

    Updated site

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-rm www/*                                         # Just making sure it's clear
-harp compile harp/ www/ &                        # Compile harp
-build_pid=$!                                     # Grabbing pid of harp process 
-wait $build_pid                                  # Waiting until harp's compiled
-ls www/*                                         # Making sure there's stuff in www
-git add -A && git commit -m 'Updated site'       # Commit and update
-echo "Harp compiled, pushing to subtree"         # Notifiying user
-git push
-#git subtree push --prefix www origin gh-pages    # Pushing compiled site to gh-pages
-
+rm www/*                                                  # Just making sure it's clear
+harp compile harp/ www/ &                                 # Compile harp
+build_pid=$!                                              # Grabbing pid of harp process 
+wait $build_pid                                           # Waiting until harp's compiled
+ls www/*                                                  # Making sure there's stuff in www
+git add -A && git commit -m 'Updated site'                # Commit and update
+echo "Harp compiled, pushing to subtree"                  # Notifiying user
+git push && git subtree push --prefix www origin gh-pages # Pushing compiled site to gh-pages
+git checkout gh-pages && git pull                         # Pulling site changes to local
+git checkout gh-pages-dev && git pull                     # Pulling dev changes to local
